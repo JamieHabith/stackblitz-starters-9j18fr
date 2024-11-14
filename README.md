0df71c096a5ed3c535cdd52230a9eb8a4d3f2c15
JamieHabith <jamiehabith@gmail.com>
2024-11-14 14:21:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-9j18fr
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/JamieHabith/stackblitz-starters-9j18fr)
---
+++ script.js
@@ -0,0 +1,27 @@
+//** Zahlen
+x = 17;
+y = 4;
+z = x + y;
+
+document.write(z);
+
+document.write('<br>');
+
+//** Texte
+t1 = 'Jochen spielt';
+t2 = 'Dokkan';
+
+document.write(t1 + ' ' + t2);
+
+document.write('<br>');
+
+document.write(document.title);
+
+//** Fallunterscheidungen
+G = 1;
+
+if(G == 1) (document.write('Rot'));
+console.log('Rot');
+
+else( document.write('Blau'));
+console.log('Blau');
