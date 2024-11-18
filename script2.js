a779b669e259657cad38f5ed8a807fcbeb7d34a4
JamieHabith <jamiehabith@gmail.com>
2024-11-18 10:25:20 +0100

    neue version

--- script2.js
+++ script2.js
@@ -5,5 +5,3 @@ Stapel = new Array(AL[0], AL[1], AL[0], AL[1], AL[0], AL[0], AL[0], AL[0], AL[0]
 L = Stapel.lenght;
 
 for (i = 0; i < L; i++) { document.getElementById("PA").innerHTML += Stapel[i] + "<br>;" }
-
-alert(Stapel)
